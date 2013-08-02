80c4623a3935b2598d2db5a35e76aa2c63ccabc4
Clay <c.ewing@miami.edu>
2013-08-02 15:33:30 +0000

    Initial

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>iQUEST</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="css/custom.css" rel="stylesheet" media="screen">
+  </head>
+  <body>
+  <body>
+
+
+    <div class="container">
+
+      <div class="panel">
+        <h1>Map Here</h1>
+      </div>
+
+      <div class="body-content">
+
+        <div class="row">
+          <ul class="nav nav-tabs">
+            <li><a data-toggle="tab" href="#color">Water Color</a></li>
+            <li><a data-toggle="tab" href="#taste">Taste/Odor</a></li>
+            <li><a data-toggle="tab" href="#particles">Particles</a></li>
+            <li><a data-toggle="tab" href="#flow">Hours of Flow</a></li>
+          </ul>
+          <div id="charts" class="tab-content">
+            <div class="tab-pane" id="color">Color</div>
+            <div class="tab-pane" id="taste">Taste</div>
+            <div class="tab-pane" id="particles">Particles</div>
+            <div class="tab-pane" id="flow">Flow</div>
+          </div>
+        </div>
+
+        <hr>
+
+        <footer>
+        </footer>
+      </div>
+
+    </div> <!-- /container -->
+
+  
+</body>    <!-- JavaScript plugins (requires jQuery) -->
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Optionally enable responsive features in IE8 -->
+    <script src="js/respond.js"></script>
+  </body>
+</html>
